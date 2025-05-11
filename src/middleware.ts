220567b96c5aa6da814cc021e642b70d84ec61d4
Piscode <princetonino.piscos@g.msuiit.edu.ph>
2025-05-11 20:42:17 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,8 @@ import { getToken } from 'next-auth/jwt'
 
 export async function middleware(req: NextRequest)
 {
-    const secret = process.env.NEXTAUTH_SECRET!
-    // console.log('secret: ', secret)
-    const token = await getToken({ req, secret })
-    console.log('token: ', token)
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
     // const { pathname } = req.nextUrl
-
 
     if (!token)
         return NextResponse.redirect(new URL('/login', req.url))
