ced0a6ea941ab4b4b4963f601f0d3bb2092f4248
Piscode <princetonino.piscos@g.msuiit.edu.ph>
2025-05-11 20:36:32 +0800

    UPDATE:     - Enable Node.js middleware 	modified:   next.config.ts 	modified:   src/middleware.ts

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,9 @@ const nextConfig: NextConfig =
       },
     ],
   },
+  experimental: {
+    nodeMiddleware: true,
+  },
 }
 
 export default nextConfig
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,8 @@ import { getToken } from 'next-auth/jwt'
 export async function middleware(req: NextRequest)
 {
     const secret = process.env.NEXTAUTH_SECRET!
-    console.log('secret: ', secret)
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! })
+    // console.log('secret: ', secret)
+    const token = await getToken({ req, secret })
     console.log('token: ', token)
     // const { pathname } = req.nextUrl
 
